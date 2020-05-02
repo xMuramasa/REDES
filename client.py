c872a0950310b61ffb7e1224d3100a04bed99432
Martin Salinas Scussolin <martin.salinas.scussolin@gmail.com>
2020-05-01 22:05:07 -0400

    funcionando
    Co-authored-by: Senrox <senroxpvp@gmail.com>

--- client.py
+++ client.py
@@ -1,46 +1,60 @@
-
 import socket as s
 
+# variables de servidor
 serverDir = "localhost"
-serverPort = 50366
-
-socketCliente = s.socket(s.AF_INET, s.SOCK_STREAM)
-#funcion que realiza handshake
-socketCliente.connect((serverDir, serverPort))
-
-toSend = input("Ingrese URLE")
-socketCliente.send(toSend.encode())
-
-msg = socketCliente.recv(2048).decode()
-print(msg)
-socketCliente.close()
+tcpServerPort = 51366
+udpServerPort = 0
 
 
+while True:
+
+    # TCP client
+    # handshake
+    tcpSocketClient = s.socket(s.AF_INET, s.SOCK_STREAM)
+    tcpSocketClient.connect((serverDir, tcpServerPort))
+
+    # recepcion url a buscar en el servidor
+    toSend = input("Ingrese URLE: ")
+    if toSend == "terminate":
+        tcpSocketClient.send(toSend.encode())
+        print("Se corto la wea")
+        break
+
+    # envio mensaje a servidor
+    tcpSocketClient.send(toSend.encode())
+
+    # recepcion de puerto UDP from server
+    newPort = tcpSocketClient.recv(2048).decode()
+    print("El puerto a usar para UDP es:", newPort)
+
+    # cast a int para usar puerto udp
+    udpServerPort = int(newPort)
+
+   
+   
+   
+    # UDP client
+    # Inicio conexion UDP
+    udpSocketClient  = s.socket(s.AF_INET, s.SOCK_DGRAM)
+    print("")   
+    toSend = input('Enviar: ')
+    while toSend.lower() != "ok":
+        toSend = input("Enviar ok plz: ")
+        
+    udpSocketClient.sendto(toSend.encode(), (serverDir, udpServerPort))
+
+    # recepcion header
+    messageToDecode, _ = udpSocketClient.recvfrom(2048)
+    message = messageToDecode.decode()
+    if message != "" and message != "Escriba OK  >_<":
+        print("Header Recibido.")
+        
+    elif message == "Escriba OK  >_<":
+        print("\n"+message)
+    
+    # cierre conexion UDP
+    udpSocketClient.close()
+    # cierre conexion TCP
+    tcpSocketClient.close()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-serverDir = 'localhost'
-serverPort = 50365
-
-socketClient  = s.socket(s.AF_INET, s.SOCK_DGRAM)
-
-toSend = input('Ingresar texto a convertir: ')
-socketClient.sendto(toSend.encode(), (serverDir, serverPort))
-
-message, _ = socketClient.recvfrom(2048)
-print(message.decode())
-socketClient.close()
-"""
--- server.py
+++ server.py
@@ -1,51 +1,83 @@
 import socket as s
 
-
-# TCP 
+# TCP Server
 # AF_INET -> IPV4
 # socketURL.send(b"GET / HTTP/1.1\r\nHost: {URL}\r\n\r\n")
-
 # puerto que utiliza el serverpara comunicarse
+# 49152 .. 65535
 serverDir = 'localhost'
-serverPort = 50366 
+serverPort = 51366
+newServerPort = serverPort + 10
 
+#sockets para conexion TCP
 tcpSocketServ = s.socket(s.AF_INET, s.SOCK_STREAM)
 tcpSocketServ.bind(('', serverPort))
+
 # ya su handshake
-tcpSocketServ.listen(1)
-print("TCP Server listening to port", serverPort)
+tcpSocketServ.listen(2)
+print("Servidor TCP escuchando en el puerto: ", serverPort)
+
+cache = []
 
 while 1:
+    # Aceptar el mensaje del clienteTCP a servidorTCP
     tcpSocketClient, clientDir = tcpSocketServ.accept()
-
     message = tcpSocketClient.recv(2048).decode()
+
+    # Terminar comunicacion
     if message == "terminate":
+        print("Cago el server")
         break
-    response = message.upper()
 
-    print("se reviejde", message)
-    tcpSocketClient.send(message.encode())
+    # Hacer el GET
+    socketUrl = s.socket(s.AF_INET, s.SOCK_STREAM)
+    socketUrl.connect((message, 80))   
+    socketUrl.sendall(b'GET / HTTP/1.1\r\n\r\n')
+    
+    # recibir respuesta GET
+    headReceived = socketUrl.recv(2048)
+    header = headReceived.decode("cp437")
+    
+    # split header para cliente
+    header = header.split("<")[0]
+    cache.append(header)
+
+    # respuesta servidor -> cliente
+    toSend = str(newServerPort)
+    tcpSocketClient.send(toSend.encode())
+    print("Envio puerto UDP a cliente")
+    
+    # cierre de socket usados en TCP
+    socketUrl.close()
     tcpSocketClient.close()
+    
+    # Inicio conexion UDP 
 
-"""
-# UDP SERVER
-# 49152 .. 65535
-serverPort = 50365
-# SOCK_DGRAM -> indica UDP
-UdpSocketServ  = s.socket(s.AF_INET, s.SOCK_DGRAM)
+    # Sockets para conexion UDP
+    udpSocketServ = s.socket(s.AF_INET, s.SOCK_DGRAM) # SOCK_DGRAM -> indica UDP
+    udpSocketServ.bind(('', newServerPort))
 
-UdpSocketServ.bind(('', serverPort))
+    print('\nServidor UDP escuchando en el puerto:', newServerPort)
+    print("Esperando respuesta cliente")
 
-print('Servidor escuchando en puerto:', serverPort)
-
-while 1:
-    message, clientDir = UdpSocketServ.recvfrom(2048)
+    message, clientDir = udpSocketServ.recvfrom(2048)
     decoded = message.decode()
+    if decoded == "terminate":
+        print("Cago el server")
+        break
 
-    print('Se recibio: ', decoded)
+    print('Respuesta cliente: ', decoded)
     respuesta = message.decode()
 
-    answer = 'Answer: '+ decoded.upper()
-    socketServ.sendto(answer.encode(), clientDir)
+    answer = 'Respuestaaaaaa: '+ header
+    udpSocketServ.sendto(answer.encode(), clientDir)
+        
+    # cierre server UDP
+    print("Cierre servidor UDP")
+    udpSocketServ.close()
+            
 
-"""
+# cierre conexion TCP
+tcpSocketClient.close()
+tcpSocketServ.close()
+
